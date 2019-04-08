963534dad8c693c7a6d0bb22f1fa71e7199f0784
nextmyhouse <35440129+nextmyhouse@users.noreply.github.com>
2019-04-08 17:09:12 +0900

    Delete caiser.py

--- caiser.py
+++
@@ -1,8 +0,0 @@
-def caesar(plain_text ,movenum = 13):
-  maincode = list(plain_text)
-  password = '' h
-  for list_code in maincode:
-   ASCI = ord(code_list) + movenum
-   password += chr(code_ASC)
-  return password
-print caesar("ddfdfsv5hf",5)
