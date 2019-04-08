c9d7e9b573f1935f57e0c912e2e939fb33a0e90a
nextmyhouse <35440129+nextmyhouse@users.noreply.github.com>
2019-04-08 14:13:04 +0900

    Add files via upload

--- postfix calculator.py
+++ postfix calculator.py
@@ -1,11 +1,15 @@
 in_num = input("식을 입력하시오:(띄어쓰기 필수)").split()
-operator = ["+", "-", "*", "/"]
+operator = ["+", "-", "*", "/", "(", ")"]
 postfix = []
 stack = []
 
 def operator_num(n): #기호에 따른 우선 순위 파악을 위한 함수
     if n == "+" or n == "-":
         return 1
+    elif n == "(":
+        return 3
+    elif n == ")":
+        return 4
     else:
         return 2
 
@@ -39,27 +43,50 @@ def post_fix(*args): #중위식을 넣어서 후위식으로 변경한다.
                 stack_value = operator_num(stack[-1])#스택의 값을 비교위해 변환한다.
 
                 if operator_value > stack_value: #들어오는 기호의 우선순위가 스택에 있는 기호보다 높을 경우
-                    stack.append(arg) #스택에 그대로 저장한다.
+                    if operator_value == 4:
+                        while True:
+                            postfix.append(stack.pop())
+                            if stack[-1] == "(":
+                                stack.pop()
+                                break
+
+                    else:
+                        stack.append(arg) #스택에 그대로 저장한다.
+
 
                 elif operator_value < stack_value: #들어오는 기호의 우선순위가 스택의 기호보다 낮을 경우
-                    while len(stack) > 0: # 스택의 값을 전부 꺼내기 위함이다.
-                        postfix.append(stack.pop()) #스택의 값들을 후위식에 스택식으로 쌓는다.
-                    stack.append(arg) #스택에는 우선 순위 낮은 값이 들어간다.
+                    if stack[-1] == '(':
+                        stack.append(arg)
+                    else:
+                        while len(stack) > 0: # 스택의 값을 전부 꺼내기 위함이다.
+                            if stack[-1] == 3:
+                                break
+                            postfix.append(stack.pop()) #스택의 값들을 후위식에 스택식으로 쌓는다.
+                        stack.append(arg) #스택에는 우선 순위 낮은 값이 들어간다.
+
 
                 elif operator_value == stack_value: #들어오는 기호와 스택 기호의 우선 순위가 같을 경우
-                    postfix.append((stack.pop())) #스택을 제거하고 후위식에 넣는다.
-                    stack.append(arg)
+                    if stack_value == 3:
+                        stack.append(arg)
+                    else:
+                        postfix.append((stack.pop())) #스택을 제거하고 후위식에 넣는다.
+                        stack.append(arg)
 
     while len(stack) > 0: #남은 스택을 전부 끄집어 낸다
         postfix.append(stack.pop())
+
     return postfix
 
 def calculator_function(*args): #계산하는 함수
 
     while len(postfix) > 1: #후위식이 결과 하나만 나올때까지만 반복한다.
+
         for operate in args:
+
             if operate in operator:#연산 기호 여부 확인한다.
                 arg_index = postfix.index(operate) #후위식에서 연산기호의 위치를 특정한다.
+                print("후위식: ", end='') #줄바꿈 캔슬
+                print(postfix)
                 #연산기호기준 앞의 두 숫자를 계산한다.
 
                 if operate == "+":
@@ -86,14 +113,8 @@ def calculator_function(*args): #계산하는 함수
 
             else:
                 continue
+
     return postfix
-
-
-
-
-
-
-
 
 print(post_fix(*in_num))
 print(calculator_function(*postfix))
